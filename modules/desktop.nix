2862720a869efc3d1f7125e54c5b1d7f1f8eb45e
VictoryTek <victorytech@proton.me>
2026-03-25 14:40:54 -0500

    Update desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -167,7 +167,7 @@
       };
       "org/gnome/desktop/interface" = {
         clock-format = "12h";
-        cursor-size  = 24;
+        cursor-size  = lib.gvariant.mkInt32 24;
         cursor-theme = "Bibata-Modern-Classic";
         icon-theme   = "kora";
       };
