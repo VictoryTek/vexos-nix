14e2bc925ea16ee1b606ffd393019cd764649715
VictoryTek <victorytech@proton.me>
2026-04-15 14:22:04 -0500

    Update home-htpc.nix

--- home-htpc.nix
+++ home-htpc.nix
@@ -15,10 +15,11 @@
     "org/gnome/shell" = {
       favorite-apps = [
         "com.brave.Browser.desktop"
-        "io.gitlab.librewolf-community.desktop"
+        "app.zen_browser.zen.desktop"
         "tv.plex.PlexDesktop.desktop"
         "io.freetubeapp.FreeTube.desktop"
         "org.gnome.Nautilus.desktop"
+        "io.github.up.desktop"
         "com.mitchellh.ghostty.desktop"
         "system-update.desktop"
       ];
@@ -37,6 +38,54 @@
 
     "org/gnome/desktop/screensaver" = {
       lock-enabled = false;
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Office" "Utilities" "System" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Office" = {
+      name = "Office";
+      apps = [
+        "org.onlyoffice.desktopeditors.desktop"
+        "org.gnome.TextEditor.desktop"
+        "org.gnome.Papers.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      name = "Utilities";
+      apps = [
+        "com.mattjakeman.ExtensionManager.desktop"
+        "it.mijorus.gearlever.desktop"
+        "org.gnome.tweaks.desktop"
+        "io.github.flattool.Warehouse.desktop"
+        "io.missioncenter.MissionCenter.desktop"
+        "com.github.tchx84.Flatseal.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/System" = {
+      name = "System";
+      apps = [
+        "org.pulseaudio.pavucontrol.desktop"
+        "rog-control-center.desktop"
+        "io.missioncenter.MissionCenter.desktop"
+        "org.gnome.Settings.desktop"
+        "org.gnome.seahorse.Application.desktop"
+        "nixos-manual.desktop"
+        "cups.desktop"
+        "blivet-gui.desktop"
+        "blueman-manager.desktop"
+        "btop.desktop"
+        "ca.desrt.dconf-editor.desktop"
+        "org.gnome.baobab.desktop"
+        "org.gnome.DiskUtility.desktop"
+        "org.gnome.font-viewer.desktop"
+        "org.gnome.Logs.desktop"
+        "btrfs-assistant.desktop"
+        "org.gnome.SystemMonitor.desktop"
+      ];
     };
   };
 
