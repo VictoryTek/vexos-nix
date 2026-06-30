f2a0e9c0a42fdc649dfe8047c4feed85ef86a5ce
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2026-06-29 20:45:56 -0500

    Update appimage.nix

--- modules/appimage.nix
+++ modules/appimage.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.appimage = {
     enable = true;
     binfmt = true;
+    package = pkgs.appimage-run.override {
+      extraPkgs = p: [ p.libdeflate ];
+    };
   };
 }
